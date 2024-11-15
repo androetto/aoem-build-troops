fe74af736d756d33493d6a5c51ae36d42086f053
Franco Androetto <androettofranco@gmail.com>
2024-11-15 15:48:09 -0300

    feat: add more hero

--- src/heroesData.js
+++ src/heroesData.js
@@ -7,7 +7,7 @@ import Kaso from "./assets/Kaso.webp";
 import KingArthur from "./assets/King_Arthur.webp";
 import LiDaoxuan from "./assets/Li_daoxuan.webp";
 import Thanius from "./assets/Thanius.webp";
-import WeWei from "./assets/Wu_Wei.webp";
+import WuWei from "./assets/Wu_Wei.webp";
 import Leonidas from "./assets/LeonidasI_013.webp";
 import Leon from "./assets/Leon.webp";
 import Luki from "./assets/Luki.webp";
@@ -19,35 +19,74 @@ import Tribhuwana from './assets/Tribhuwana.webp';
 import YiSunShin from './assets/Yi_Sun-Shin.webp';
 import Yodit from './assets/Yodit.webp';
 import YuanXia from './assets/Yuan_Xia.webp';
+import DariusTheGreat from './assets/Darius_the_Great.webp';
+import Cleopatra from './assets/CleopatraVII.webp';
+import Ashoka from './assets/Ashoka.webp';
+import ConstantineTheGreat from './assets/Constantine_the_Great.webp';
+import RaniDurgavati from './assets/Rani_Durgavati.webp';
+import FederickBarbarossa from './assets/Frederick_Barbarossa.webp';
+import MiyamotoMusashi from './assets/Miyamato_Musashi.webp';
+import QueenSeondeok from './assets/Queen_Seondeok.webp';
+import Octavian from './assets/Octavian.webp';
+import no from './assets/no.jpeg'
 
 export const heroesList = {
   mythics: [{ id: 1, name: "King Arthur", image: KingArthur }],
   legendaries: [
     { id: 2, name: "Josephine", image: Josephine },
     { id: 3, name: "Juan of arc", image: JoanOfArc },
-    { id: 4, name: "Guan Yu", image: GuanYu },
-    { id: 5, name: "Harald |||", image: Harald3 },
-    { id: 6, name: "Kaso", image: Kaso },
-    { id: 7, name: "Hummurabi", image: Hammurabi },
-
-    { id: 9, name: "Thanius", image: Thanius },
-    { id: 10, name: "Leonidas", image: Leonidas },
-
-    { id: 13, name: "Luki", image: Luki },
-    { id: 14, name: "Lu Bu", image: LuBu },
-    { id: 15, name: "Ram", image: RamKhamhaeng },
-
-    { id: 16, name: "Tribhuwana", image: Tribhuwana },
-    { id: 17, name: "Yi Sun Shin", image: YiSunShin },
-    { id: 18, name: "Yodit", image: Yodit },
-    { id: 19, name: "Yuan Xia", image: YuanXia },
+    { id: 4, name: "Leonidas |", image: Leonidas },
+    { id: 5, name: "Darius the Great", image: DariusTheGreat },
+    { id: 6, name: "Cleopatra", image: Cleopatra},
+    { id: 7, name: "Yi Seong-Gye", image: no},
+    { id: 8, name: "Yi Sun Shin", image: YiSunShin },
+    { id: 9, name: "King Derrick", image: no },
+    { id: 10, name: "Harald |||", image: Harald3 },
+    { id: 11, name: "Toyotomi Hideyoshi", image: no },
+    { id: 12, name: "Guan Yu", image: GuanYu},
+    { id: 13, name: "Tokugawa Ieyasu", image: no},
+    { id: 14, name: "Julio Cesar", image: no},
+    { id: 15, name: "Hummurabi", image: Hammurabi },
+    { id: 16, name: "Oda Nobunaga", image: no},
+    { id: 17, name: "Atila el huno", image: no},
+    { id: 18, name: "Ashoka", image: Ashoka},
+    { id: 19, name: "Constantine", image: ConstantineTheGreat},
+    { id: 20, name: "Diao Chang", image: no},
+    { id: 21, name: "Teodora", image: no},
+    { id: 22, name: "Rani Durgavati", image: RaniDurgavati},
+    { id: 23, name: "Felipe VI", image: no},
+    { id: 24, name: "Federick Barbarossa", image: FederickBarbarossa},
+    { id: 25, name: "Federick Barbarossa", image: FederickBarbarossa},
+    { id: 26, name: "Miyamato Musashi", image: MiyamotoMusashi},
+    { id: 27, name: "Tariq", image: no},
+    { id: 28, name: "Anibal", image: no},
+    { id: 29, name: "Constantine", image: ConstantineTheGreat},
+    { id: 30, name: "Ricardo |", image: no},
+    { id: 31, name: "Sejong", image: no},
+    { id: 32, name: "Queen Seondeok", image: QueenSeondeok},
+    { id: 33, name: "Bushra", image: no},
+    { id: 34, name: "Sun Tzu", image: SunTzu},
+    { id: 34, name: "Tribhuwana", image: Tribhuwana},
+    { id: 35, name: "Hua Mulan", image: no},
+    { id: 36, name: "Octavian", image: Octavian},
+    { id: 37, name: "Cid", image: no},
   ],
   epics: [
-    { id: 20, name: "Li Daoxuan", image: LiDaoxuan },
-    { id: 21, name: "We Wei", image: WeWei },
-    { id: 22, name: "Leon", image: Leon },
-    { id: 23, name: "Narses", image: Narses },
-    { id: 24, name: "Sun Tzu", image: SunTzu },
-    { id: 25, name: "Sun Tzu", image: SunTzu },
+    { id: 38, name: "Kaso", image: Kaso },
+    { id: 39, name: "Narses", image: Narses },
+    { id: 40, name: "Ciu Ruyi", image: no },
+    { id: 41, name: "Axel", image: no },
+    { id: 42, name: "Li Daoxuan", image: LiDaoxuan },
+    { id: 43, name: "Leon", image: Leon },
+    { id: 44, name: "Thanus", image: Thanius },
+    { id: 45, name: "Baldassi", image: no },
+    { id: 46, name: "Gatos", image: no },
+    { id: 47, name: "Luki", image: Luki },
+    { id: 48, name: "Gao Meng", image: no },
+    { id: 49, name: "Clyde", image: no },
+    { id: 50, name: "Leon", image: Leon },
+    { id: 51, name: "Nino", image: no },
+    { id: 52, name: "Wu Wei", image: WuWei },
+    { id: 53, name: "Tuan Xia", image: no },
   ],
 };
